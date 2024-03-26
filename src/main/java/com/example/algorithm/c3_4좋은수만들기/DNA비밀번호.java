2ebf7d6d0a8a07f61051c55f60316a2937fb7166
Lazy Cat <mylazysundaycat@gmail.com>
2024-03-27 00:34:17 +0900

    백준 12891 슬라이딩 처리 후 오류

--- src/main/java/com/example/algorithm/c3_4좋은수만들기/DNA비밀번호.java
+++ src/main/java/com/example/algorithm/c3_4좋은수만들기/DNA비밀번호.java
@@ -14,43 +14,89 @@ public class DNA비밀번호 {
         int S = Integer.parseInt(st.nextToken());
         int P = Integer.parseInt(st.nextToken());
 
-        //DNA 문자열
-        char[] DNA = new char[S];
-        DNA = bf.readLine().toCharArray();
+        int result = 0;
+
+        char[] A = bf.readLine().toCharArray();
+        int[] checkarr = new int[4];
+        int[] myArr = new int[4];
+        int checkSecret=0;
 
         //부분 문자열에 포함돼야할 A,C,G,T 갯수
-        int[] dnaNum = new int[4];
         st = new StringTokenizer(bf.readLine());
         for (int i = 0; i < 4; i++) {
-            dnaNum[i] = Integer.parseInt(st.nextToken());
-        }
-        int start=0;
-        int end =P;
-        int result=0;
-
-        while(start<=S-P){
-            int[] checkNum = new int[4];
-            for(int i=start; i<end; i++){
-                if(DNA[i] == 'A'){
-                    checkNum[0]++;
-                } else if(DNA[i] == 'C') {
-                    checkNum[1]++;
-                } else if(DNA[i] == 'G') {
-                    checkNum[2]++;
-                } else if(DNA[i] == 'T') {
-                    checkNum[3]++;
-                }
-            }
-            //검증
-            if(checkNum[0]==dnaNum[0] && checkNum[1]==dnaNum[1] &&
-            checkNum[2]==dnaNum[2] && checkNum[3]==dnaNum[3]){
-                result++;
-            }
-            start++;
-            end++;
+            checkarr[i] = Integer.parseInt(st.nextToken());
+            if(checkarr[i]==0) checkSecret++;
         }
 
+
+        //초기 배열 P 처리부분
+        for (int i = 0; i < P; i++) {
+            switch(A[i]){
+                case 'A':
+                    myArr[0]++;
+                    if(myArr[0]==checkarr[0]) checkSecret++;
+                    break;
+                case 'C':
+                    myArr[1]++;
+                    if(myArr[1]==checkarr[1]) checkSecret++;
+                    break;
+                case 'G':
+                    myArr[2]++;
+                    if(myArr[2]==checkarr[2]) checkSecret++;
+                    break;
+                case 'T':
+                    myArr[3]++;
+                    if(myArr[3]==checkarr[3]) checkSecret++;
+                    break;
+            }
+        }
+
+        //슬라이딩 윈도우
+        for(int i=P; i<S; i++){
+            int j = i-P;
+            //더하기
+            switch(A[i]){
+                case 'A':
+                    myArr[0]++;
+                    if(myArr[0]==checkarr[0]) checkSecret++;
+                    break;
+                case 'C':
+                    myArr[1]++;
+                    if(myArr[1]==checkarr[1]) checkSecret++;
+                    break;
+                case 'G':
+                    myArr[2]++;
+                    if(myArr[2]==checkarr[2]) checkSecret++;
+                    break;
+                case 'T':
+                    myArr[3]++;
+                    if(myArr[3]==checkarr[3]) checkSecret++;
+                    break;
+            }
+            //빼기
+            switch(A[j]){
+                case 'A':
+                    if(myArr[0]==checkarr[0]) checkSecret--;
+                    myArr[0]--;
+                    break;
+                case 'C':
+                    if(myArr[1]==checkarr[1]) checkSecret--;
+                    myArr[1]--;
+                    break;
+                case 'G':
+                    if(myArr[2]==checkarr[2]) checkSecret--;
+                    myArr[2]--;
+                    break;
+                case 'T':
+                    if(myArr[3]==checkarr[3]) checkSecret--;
+                    myArr[3]--;
+                    break;
+            }
+            if(checkSecret==4) result++;
+        }
         System.out.println(result);
+        bf.close();
+
 
     }
 }
