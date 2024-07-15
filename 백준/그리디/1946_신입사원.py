a88ca6ac975757b4d366cc1620b9829021d4fd52
Lazy Cat <mylazysundaycat@gmail.com>
2024-07-15 20:25:45 +0900

    0715 백준 그리디

--- 백준/그리디/1946_신입사원.py
+++ 백준/그리디/1946_신입사원.py
@@ -13,10 +13,12 @@ for _ in range(t):
     lst.sort(key=lambda x:x[0])
 
     cnt=1
-    check = lst[i][1]
+    check = lst[0][1]
     for i in range(1,n):
         if check>lst[i][1]:
             cnt += 1
             check=lst[i][1]
 
     print(cnt)
+
+
